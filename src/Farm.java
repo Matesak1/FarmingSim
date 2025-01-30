c066383e1d38c7473f49425b1eb1d0532681b5d1
Mathew <m.matejsvarc@gmail.com>
2025-01-30 10:01:39 +0100

    added a command to add a flower to the farm to Main.java added a command to print the flowers in the farm to Main.java added a constructor to Flower.java made a new arraylist in Farm.java added a method to add a flower to the arraylist in Farm.java added a tostring method to Farm.java

--- src/Farm.java
+++ src/Farm.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+
 public class Farm {
+    ArrayList<Flower> flowers = new ArrayList<>();
+    private double velikostFarmyMetr = 10000;
+    public void zasaditRostlinu(String name, double price, double space, double growthChance){
+        if(velikostFarmyMetr >=space){
+            if(flowers.size()!=5) {
+                flowers.add(new Flower(name, price, space, growthChance) {
+                    @Override
+                    public String toString() {
+                        return super.toString();
+                    }
+                });
+                velikostFarmyMetr = velikostFarmyMetr - space;
+            }
+        }
+    }
 
-
-
+    @Override
+    public String toString() {
+        return "Farm{" +
+                "flowers=" + flowers +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        Farm farm = new Farm();
+        farm.zasaditRostlinu("obili", 5, 10, 50);
+        System.out.println(farm.toString());
     }
 }
